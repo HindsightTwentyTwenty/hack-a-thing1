df355e124a1868ab581758eda601e3b30032c4c4
Zachary Tannenbaum <zach@sighten.io>
2016-09-26 12:03:43 -0400

    updated to using angularjs

--- popup.js
+++ popup.js
@@ -1,6 +1,35 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+/**
+  * Hack-a-Thing Group 1
+  * Tommy, Grace and Zach
+  */
+
+var hindsight = angular.module('hindsight', []);
+
+
+hindsight.controller('TabsController', function($scope, $http) {
+  $scope.tabs = [];
+  $scope.hello = "hello world";
+
+  $scope.fetchTabs = function() {
+    // Query filter to be passed to chrome.tabs.query - see
+    // https://developer.chrome.com/extensions/tabs#method-query
+    var query_args = {
+      // active: true,
+      currentWindow: true
+    };
+
+    chrome.tabs.query(query_args, function(tabs) {
+      $scope.tabs = tabs;
+      console.log(tabs)
+      console.log(tabs[0].url)
+    });
+  };
+
+  $scope.apply($scope.fetchTabs());
+
+});
+
+
 
 /**
  * Get the current URL.
@@ -8,50 +37,48 @@
  * @param {function(string)} callback - called when the URL of the current tab
  *   is found.
  */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    // active: true,
-    currentWindow: true
-  };
+// function getCurrentTabUrl(callback) {
+//   // Query filter to be passed to chrome.tabs.query - see
+//   // https://developer.chrome.com/extensions/tabs#method-query
+//   var queryInfo = {
+//     // active: true,
+//     currentWindow: true
+//   };
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
+//   chrome.tabs.query(queryInfo, function(tabs) {
+//     // chrome.tabs.query invokes the callback with a list of tabs that match the
+//     // query. When the popup is opened, there is certainly a window and at least
+//     // one tab, so we can safely assume that |tabs| is a non-empty array.
+//     // A window can only have one active tab at a time, so the array consists of
+//     // exactly one tab.
 
-    var tab = tabs[0];
+//     $scope.tabs = tabs;
     
-    var url = tab.url;
 
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
 
-    callback(url);
-  });
+//     // tab.url is only available if the "activeTab" permission is declared.
+//     // If you want to see the URL of other tabs (e.g. after removing active:true
+//     // from |queryInfo|), then the "tabs" permission is required to see their
+//     // "url" properties.
 
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
+//   });
 
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
+//   // Most methods of the Chrome extension APIs are asynchronous. This means that
+//   // you CANNOT do something like this:
+//   //
+//   // var url;
+//   // chrome.tabs.query(queryInfo, function(tabs) {
+//   //   url = tabs[0].url;
+//   // });
+//   // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+// }
 
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    renderStatus(url);
-  });
-});
+// function renderStatus(statusText) {
+//   document.getElementById('status').textContent = statusText;
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   getCurrentTabUrl(function(url) {
+//     renderStatus(url);
+//   });
+// });
